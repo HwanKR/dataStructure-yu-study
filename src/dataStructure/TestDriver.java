d8953924b95c4d7c66cc659cc0d268b9d1ac8049
Jeon Seung Hwan <fabronjeon@naver.com>
2025-10-17 10:49:35 +0900

    7주차 실습자료 업데이트

--- src/dataStructure/TestDriver.java
+++ src/dataStructure/TestDriver.java
@@ -1,34 +1,17 @@
 package dataStructure;
 
+import java.util.Scanner;
+
 public class TestDriver {
+
 	public static void main(String[] args) {
-//		SinglyLinkedList<String> list = new SinglyLinkedList<>();
-//		
-//		list.addFirst("A");
-//		list.addFirst("B");
-//		list.addFirst("C");
-//		
-//		list.addLast("D");
-//		list.addLast("E");
-//		list.addLast("F");
-//		
-//		
-//		list.add(1,  "K");
-//		list.add(0,  "X");
-//		list.add(6,  "Y");
-//		
-//		System.out.println(list);
-//		
-//		list.remove(0);
-//		list.remove("E");
-//		System.out.println(list);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("파일 이름? ");
+		String fileName = sc.next();
+		sc.close();
 		
-		Stack<Integer> stack = new LinkedStack<Integer>();
-		
-		stack.push(1);
-		stack.push(3);
-		stack.push(5);
-		
-		System.out.println(stack);
+		EqClass algorithm = new EqClass();
+		algorithm.loadPairs(fileName);
+		algorithm.displayEqClass();
 	}
 }

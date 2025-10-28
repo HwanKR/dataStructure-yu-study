1b33362fe64774afc5db93c288b7b21369090240
Jeon Seung Hwan <fabronjeon@naver.com>
2025-10-28 13:17:54 +0900

    TreeNode.java 생성 및 구현

--- src/dataStructure/chap5/TreeNode.java
+++ src/dataStructure/chap5/TreeNode.java
@@ -1,5 +1,26 @@
 package dataStructure.chap5;
 
-public class TreeNode {
-
+public class TreeNode<E> implements Position<E> {
+	private E element;
+	private TreeNode<E> left, right;
+	
+	public TreeNode(E element) {
+		this.element = element;
+	}
+	
+	public TreeNode(E element, TreeNode<E> left, TreeNode<E> right) {
+		this.element = element;
+		this.left = left;
+		this.right = right;
+	}
+	
+	@Override
+	public E getElement() throws IllegalStateException {
+		return element;
+	}
+	
+	public String toString() {
+		return "노드(" + element + ")";
+	}
+	
 }

9d414ce01435adb6c219f0ff96e0859a50252c18
Jeon Seung Hwan <fabronjeon@naver.com>
2025-10-31 08:15:21 +0900

    Chap5 LinkedStack 생성

--- src/dataStructure/chap5/LinkedStack.java
+++ src/dataStructure/chap5/LinkedStack.java
@@ -1,5 +1,36 @@
 package dataStructure.chap5;
 
-public class LinkedStack implements Stack<E> {
-
+public class LinkedStack<E> implements Stack<E> {
+	SinglyLinkedList<E> list = new SinglyLinkedList<>();
+	
+	public int size() {
+		return list.size();
+	}
+	
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+	
+	public boolean isFull() {
+		return false;
+	}
+	
+	public void push(E e) {
+		list.addFirst(e);
+	}
+	
+	public E top() {
+		if (isEmpty()) 
+			return null;
+		return list.get(0);
+	}
+	
+	public E pop() {
+		if (isEmpty())
+			return null;
+		E item = top();
+		list.remove(0);
+		return item;
+	}
+	
 }

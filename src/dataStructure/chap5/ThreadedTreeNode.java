1047dfe7f2cfb45846b44262eea593465c23ba2e
Jeon Seung Hwan <fabronjeon@naver.com>
2025-11-05 14:03:32 +0900

    chap5 ThreadedTreeNode.java insucc 구현

--- src/dataStructure/chap5/ThreadedTreeNode.java
+++ src/dataStructure/chap5/ThreadedTreeNode.java
@@ -1,5 +1,7 @@
 package dataStructure.chap5;
 
+import java.util.ArrayList;
+
 public class ThreadedTreeNode<E> implements Position<E> {
 	private E element;
 	private boolean lthread;
@@ -44,5 +46,27 @@ public class ThreadedTreeNode<E> implements Position<E> {
 	public void setRight(ThreadedTreeNode<E> right) {
 		this.right = right;
 	}
-
+	
+	public boolean isRThread() {
+        return rthread;
+    }
+    public void setRThread(boolean rthread) {
+        this.rthread = rthread;
+    }
+    
+    public boolean isLThread() {
+        return lthread;
+    }
+    public void setLThread(boolean lthread) {
+        this.lthread = lthread;
+    }
+    
+    // 한 노드의 inorder successor 발견
+    private ThreadedTreeNode<E> insucc(ThreadedTreeNode<E> p) {
+    	ThreadedTreeNode<E> temp = p.getRight();
+    	if (p.rthread == false)
+    		while (temp.lthread == false)
+    			temp = temp.getLeft();
+    	return temp;
+    }
 }

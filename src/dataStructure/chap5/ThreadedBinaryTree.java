3f253e4e50b58000605dd86493694411cb7ce29c
Jeon Seung Hwan <fabronjeon@naver.com>
2025-11-07 08:12:42 +0900

    chap5 ThreadedBinaryTree.java: insertRight 구현

--- src/dataStructure/chap5/ThreadedBinaryTree.java
+++ src/dataStructure/chap5/ThreadedBinaryTree.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class ThreadedBinaryTree<E> {
 	protected ThreadedTreeNode<E> root;
 	
+	protected ThreadedTreeNode<E> validate(Position<E> p) {
+		if (!(p instanceof TreeNode)) {
+			throw new IllegalArgumentException("Invalid type");
+		}
+		return (ThreadedTreeNode<E>)p;
+	}
+	
     // 한 노드의 inorder successor 발견
     private ThreadedTreeNode<E> insucc(ThreadedTreeNode<E> p) {
     	ThreadedTreeNode<E> temp = p.getRight();
@@ -26,6 +33,22 @@ public class ThreadedBinaryTree<E> {
     		result.add(p);
     	}
     	return result;
-    }	
+    }
+    
+    public void insertRight(Position<E> pParent, Position<E> pChild) {
+    	ThreadedTreeNode<E> parent = validate(pParent);
+    	ThreadedTreeNode<E> child = validate(pChild);
+    	child.right = parent.right;
+    	child.rthread = parent.rthread;
+    	child.left = parent;
+    	child.lthread = true;
+    	parent.right = child;
+    	parent.rthread = false;
+    	if (child.rthread == false) {
+    		ThreadedTreeNode<E> temp = insucc(child);
+    		temp.left = child;
+    	}
+    	
+    }
     
 }

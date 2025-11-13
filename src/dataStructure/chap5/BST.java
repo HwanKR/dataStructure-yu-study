5a07a1fb7c7c655ea7be72c05195b9b3926a7746
HwanKR <fabronjeon@naver.com>
2025-11-13 21:16:51 +0900

    chap5 BST.java 재귀함수를 통한 이진 검색 트리 구현 완료. -> 기본 이진 검색트리 구성 구현 필요

--- src/dataStructure/chap5/BST.java
+++ src/dataStructure/chap5/BST.java
@@ -1,6 +1,8 @@
 package dataStructure.chap5;
 
 public class BST<K extends Comparable<K>, V> {
+	protected Node<K, V> root;
+	
 	class Node<K, V> {
 		K key;
 		V value;
@@ -9,6 +11,32 @@ public class BST<K extends Comparable<K>, V> {
 		public Node(K key, V val) {
 			this.key = key;
 			this.value = val;
+		}
+	}
+	
+	protected Node<K, V> treeSearch(K searchKey) {
+		if (root == null) return null;
+		
+		return recursiveSearchHelper(root, searchKey);
+	}
+	
+	private Node<K, V> recursiveSearchHelper(Node<K, V> currentNode, K searchKey) {
+		int cmp = searchKey.compareTo(currentNode.key);
+		
+		if (cmp == 0 || (currentNode.left == null && currentNode.right == null)) 
+			return currentNode;
+		
+		if (cmp < 0) {
+			if (currentNode.left != null) 
+				return recursiveSearchHelper(currentNode.left, searchKey);
+			else 
+				return currentNode;
+		}
+		else {
+			if (currentNode.right != null)
+				return recursiveSearchHelper(currentNode.right, searchKey);
+			else 
+				return currentNode;
 		}
 	}
 }

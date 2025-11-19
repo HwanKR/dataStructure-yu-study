187d96e81df04032826cfacbd23869e307d35fb1
HwanKR <fabronjeon@naver.com>
2025-11-19 21:51:49 +0900

    chap6: MatrixGraph.java 생성 및 구현

--- src/dataStructure/chap6/Graph.java
+++ src/dataStructure/chap6/Graph.java
@@ -4,6 +4,4 @@ public interface Graph {
 	int size();
 	void addEdge(int v, int w);
 	Iterable<Integer> adj(int v);
-	
-	test
 }
---
+++ src/dataStructure/chap6/MatrixGraph.java
@@ -0,0 +1,50 @@
+package dataStructure.chap6;
+
+import java.util.ArrayList;
+
+public class MatrixGraph implements Graph {
+	int size;
+	boolean directed;
+	int[][] adj;
+	
+	public MatrixGraph(int size, boolean directed) {
+		this.size = size;
+		this.directed = directed;
+		adj = new int[size][size];
+		
+	}
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public void addEdge(int v, int w) {
+		adj[v][w] = 1;
+		if (!directed) 
+			adj[w][v] = 1;
+	}
+
+	@Override
+	public Iterable<Integer> adj(int v) {
+		ArrayList<Integer> L = new ArrayList<Integer>(size);
+		for (int i=0; i<size; i++)
+			if (adj[v][i] == 1)
+				L.add(i);
+		return L;
+	}
+	
+	public int inDegree(int v) {
+		int count = 0;
+		for (int i=0; i<size; i++)
+			count += adj[i][v];
+		return count;
+	}
+	
+	public int outDegree(int v) {
+		int count = 0;
+		for (int i=0; i<size; i++)
+			count += adj[v][i];
+		return count;
+	}
+}
